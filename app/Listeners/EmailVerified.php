87ce8fbfe80ce607778b19460ea42332a5429092
zhongdj1 <729130967@qq.com>
2019-11-08 17:23:11 +0800

    认证后的消息提醒

---
+++ app/Listeners/EmailVerified.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class EmailVerified
+{
+    /**
+     * Handle the event.
+     *
+     * @param  Verified  $event
+     * @return void
+     */
+    public function handle(Verified $event)
+    {
+        session()->flash('success', '邮箱验证成功');
+    }
+}
