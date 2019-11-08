0aa02b1936f925d548aa3ad325803bb283cccd29
zhongdj1 <729130967@qq.com>
2019-11-08 17:53:06 +0800

    显示个人资料

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,11 +13,11 @@ class User extends Authenticatable implements MustVerifyEmailContract
 
     /**
      * The attributes that are mass assignable.
-     *
+     * 防止用户随意修改模型数据，只有在此属性里定义的字段，才允许修改，否则更新时会被忽略
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'avatar', 'introduction'
     ];
 
     /**
