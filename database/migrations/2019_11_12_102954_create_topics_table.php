d51dddd3e7f214aa35bf716065774aa2c1e1c57d
zhongdj1 <729130967@qq.com>
2019-11-12 11:13:10 +0800

    话题列表页面

--- database/migrations/2019_11_12_102954_create_topics_table.php
+++ database/migrations/2019_11_12_102954_create_topics_table.php
@@ -14,6 +14,8 @@ class CreateTopicsTable extends Migration
             $table->bigInteger('user_id')->unsigned()->index();
             $table->integer('category_id')->unsigned()->index();
             $table->integer('reply_count')->unsigned()->default(0);
+            $table->integer('view_count')->unsigned()->default(0);
+            $table->integer('last_reply_user_id')->unsigned()->default(0);
             $table->integer('order')->unsigned()->default(0);
             $table->text('excerpt')->nullable();
             $table->string('slug')->nullable();
