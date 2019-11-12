4e773b25fea380e8ec5ed98c694bbd92286c05fd
zhongdj1 <729130967@qq.com>
2019-11-12 14:56:20 +0800

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
   {{-- Styles --}}
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
+  @yield('styles')
+
 </head>
 <body>
   <div id="app" class="{{ route_class() }}-page">
@@ -33,6 +35,9 @@
 
   {{-- Scripts --}}
   <script src="{{ mix('js/app.js') }}"></script>
+
+  @yield('scripts')
+
 </body>
 
 </html>
