58786b034cfc9b1dc82fd08136b319a8ebd17f43
zhongdj1 <729130967@qq.com>
2019-11-12 15:08:18 +0800

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,4 @@ Route::resource('users', 'UsersController');
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
