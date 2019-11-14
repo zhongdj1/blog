0745ac980f39cc6f9f0b3a403bf48a64cd61e139
zhongdj1 <729130967@qq.com>
2019-11-14 11:53:39 +0800

    翻译 URL 标示

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Handlers\SlugTranslateHandler;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -13,6 +14,12 @@ class TopicObserver
     {
 //        $topic->body = clean($topic->body, 'user_topic_body');
 
+        //话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
