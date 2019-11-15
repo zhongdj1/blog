ac5d04d9c1402a6c0105b6e5521e6630e95263f9
zhongdj1 <729130967@qq.com>
2019-11-15 16:47:50 +0800

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -22,4 +22,9 @@ class ReplyObserver
 
         $reply->topic->user->notify(new TopicReplied($reply));
     }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->updateReplyCount();
+    }
 }
